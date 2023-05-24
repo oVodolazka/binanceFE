2b9a83085ec5e2bb1864d73dfea914d440018dea
kimicalilu <valera2491997@gmail.com>
2023-05-24 17:34:35 +0200

    fixes

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL:'http://localhost:3001/'
 });
 
-
 api.interceptors.request.use(function (config) {
   const token = localStorage.getItem('access_token')
   if(token) {
